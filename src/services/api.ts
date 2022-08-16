8f49c42b6f2052ddb3fcf6b95c1d83336faa27fd
Fabricio Oliveira <fabricioliveira021@gmail.com>
2022-08-16 15:27:04 -0300

    new URl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://dtmoney-l535i7y1a-fabriciooliveira1.vercel.app/api',
 })
